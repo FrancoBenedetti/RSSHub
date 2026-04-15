6f665cbf852ff6625c5b2fa35b9b625fafa6118b
Franco Benedetti <franco@growthhq.net>
2026-04-15 17:38:41 +0200

    feat: add Politicsweb feed route

---
+++ lib/routes/politicsweb/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Politicsweb',
+    url: 'politicsweb.co.za',
+    categories: ['politics'],
+    description: 'South African politics and opinion.',
+};
